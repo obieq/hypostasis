5e9ab80c917380be9b4df90c068e8add240aa7d7
James Thompson <james@plainprograms.com>
2014-01-11 12:32:28 -0700

    Implemented get, set and unset methods for Hypostasis::Connection.

--- lib/hypostasis/connection.rb
+++ lib/hypostasis/connection.rb
@@ -1,13 +1,23 @@
-class Hypostasis::Connection
-  def initialize
+require 'fdb'
 
+FDB.api_version 100
+
+class Hypostasis::Connection
+  attr_reader :database
+
+  def initialize
+    @database = FDB.open
   end
 
   def get(key)
-
+    database[key.to_s]
   end
 
   def set(key, value)
+    (database[key.to_s] = value.to_s) == value.to_s
+  end
 
+  def unset(key)
+    database.clear(key).nil?
   end
 end
--- test/connection_spec.rb
+++ test/connection_spec.rb
@@ -5,4 +5,13 @@ describe Hypostasis::Connection do
 
   it { subject.must_respond_to :get }
   it { subject.must_respond_to :set }
+  it { subject.must_respond_to :unset }
+
+  it 'supports simple getting and setting' do
+    subject.get('test_key').nil?.must_equal true
+    subject.set('test_key', 'test_value').must_equal true
+    subject.get('test_key').must_equal 'test_value'
+    subject.unset('test_key').must_equal true
+    subject.get('test_key').nil?.must_equal true
+  end
 end
