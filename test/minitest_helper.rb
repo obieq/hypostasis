e6a39c238d83120455bd7a1d4643e55e8fb2b7d0
James Thompson <james@plainprograms.com>
2014-01-11 12:34:41 -0700

    Added use of SimpleCov for local code coverage reporting. [ci skip]

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,6 +1,11 @@
 if ENV['CI']
   require 'codeclimate-test-reporter'
   CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/test/'
+  end
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
