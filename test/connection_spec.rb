3e442ecf93e525ad42b3473c78baef1bb6197e00
James Thompson <james@plainprograms.com>
2014-01-13 10:05:01 -0700

    Refactored Connection specs.

--- test/connection_spec.rb
+++ test/connection_spec.rb
@@ -3,15 +3,44 @@ require 'minitest_helper'
 describe Hypostasis::Connection do
   let(:subject) { Hypostasis::Connection.new }
 
+  before :each do
+
+    subject.set('unset_key', 'unset_value')
+  end
+
+  after :each do
+
+    subject.unset('unset_key')
+  end
+
   it { subject.must_respond_to :get }
   it { subject.must_respond_to :set }
   it { subject.must_respond_to :unset }
 
-  it 'supports simple getting and setting' do
-    subject.get('test_key').nil?.must_equal true
-    subject.set('test_key', 'test_value').must_equal true
-    subject.get('test_key').must_equal 'test_value'
-    subject.unset('test_key').must_equal true
-    subject.get('test_key').nil?.must_equal true
+  describe '#get and #set' do
+    before :each do
+      subject.set('preset_key', 'preset_value')
+    end
+
+    after :each do
+      subject.unset('preset_key')
+    end
+
+    it { subject.get('invalid_key').nil?.must_equal true }
+    it { subject.get('preset_key').must_equal 'preset_value' }
+  end
+
+  describe '#unset' do
+    it 'unsets a previously set key' do
+      subject.set('preset_key', 'preset_value')
+      subject.get('preset_key').must_equal 'preset_value'
+      subject.unset('preset_key').must_equal true
+      subject.get('preset_key').nil?.must_equal true
+    end
+
+    it 'returns true for an unset key' do
+      subject.get('preset_key').nil?.must_equal true
+      subject.unset('preset_key').must_equal true
+    end
   end
 end
