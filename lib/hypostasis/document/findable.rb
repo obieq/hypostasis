aabf2372b19e4e75b9f342a3f16cc340a8b708f3
James Thompson <jamest@thereadyproject.com>
2014-01-28 15:49:38 -0700

    Extracted logic for reconstituting document from keys into separate method in Hypostasis::Document::Findable.

--- lib/hypostasis/document/findable.rb
+++ lib/hypostasis/document/findable.rb
@@ -4,23 +4,11 @@ module Hypostasis::Document
 
     module ClassMethods
       def find(id)
-        document_keys = []
-        namespace.transact do |tr|
-          document_keys = tr.get_range_start_with(namespace.for_document(self, id)).to_a
+        document_keys = namespace.transact do |tr|
+          tr.get_range_start_with(namespace.for_document(self, id)).to_a
         end
         raise Hypostasis::Errors::DocumentNotFound if document_keys.empty?
-        attributes = {}
-        id = Hypostasis::Tuple.unpack(document_keys.first.key.split('\\')[1]).to_a[1]
-        document_keys.each do |key|
-          attribute_tuple = key.key.split('\\')[2]
-          next if attribute_tuple.nil?
-          unpacked_key = Hypostasis::Tuple.unpack(attribute_tuple)
-          raw_value = key.value
-          attributes[unpacked_key.to_a[0].to_sym] = reconstitute_value(unpacked_key, raw_value)
-        end
-        document = self.new(attributes)
-        document.set_id(id)
-        document
+        reconstitute_document(document_keys)
       end
 
       def find_where(field_value_pairs)
@@ -35,6 +23,20 @@ module Hypostasis::Document
         results.select! {|e| results.count(e) == field_value_pairs.size}
         results.uniq!
         results.collect! {|result| find(result) }
+      end
+
+      def reconstitute_document(keys)
+        attributes = {}
+        keys.each do |key|
+          attribute_tuple = key.key.split('\\')[2]
+          next if attribute_tuple.nil?
+          unpacked_key = Hypostasis::Tuple.unpack(attribute_tuple)
+          raw_value = key.value
+          attributes[unpacked_key.to_a[0].to_sym] = reconstitute_value(unpacked_key, raw_value)
+        end
+        document = self.new(attributes)
+        document.set_id(Hypostasis::Tuple.unpack(keys.first.key.split('\\')[1]).to_a[1])
+        document
       end
     end
   end
