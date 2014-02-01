f0025cf38714442e4cea8a3eed84d6165cf54724
James Thompson <james@plainprograms.com>
2014-02-01 16:50:31 -0700

    Fixed misnamed variables.

--- lib/hypostasis/data_models/for_indexes.rb
+++ lib/hypostasis/data_models/for_indexes.rb
@@ -2,10 +2,10 @@ module Hypostasis::DataModels::ForIndexes
   def for_index(object, field_name, value)
     index_path = Hypostasis::Tuple.new('indexes', get_class_name(object)).to_s
     value = value.to_s unless value.is_a?(Fixnum) || value.is_a?(Bignum)
-    if document.is_a?(Class)
+    if object.is_a?(Class)
       field_path = Hypostasis::Tuple.new(field_name.to_s, value).to_s
     else
-      field_path = Hypostasis::Tuple.new(field_name.to_s, value, document.id.to_s).to_s
+      field_path = Hypostasis::Tuple.new(field_name.to_s, value, object.id.to_s).to_s
     end
     name.to_s + '\\' + index_path + '\\' + field_path
   end
