79d50a78739d0d5b72d5a0a8f1effb617d431154
James Thompson <jamest@thereadyproject.com>
2014-02-07 16:49:00 -0700

    Trimmed reconstitute_value method from Hypostasis::DataModels::Utilities.

--- lib/hypostasis/data_models/utilities.rb
+++ lib/hypostasis/data_models/utilities.rb
@@ -8,32 +8,4 @@ module Hypostasis::DataModels::Utilities
   def get_object_id(object, id = nil)
     id.nil? ? object.id.to_s : id.to_s
   end
-
-  def reconstitute_value(tuple, raw_value)
-    data_type = tuple.to_a.last
-    case data_type
-      when 'Fixnum'
-        Integer(raw_value)
-      when 'Bignum'
-        Integer(raw_value)
-      when 'Float'
-        Float(raw_value)
-      when 'String'
-        raw_value
-      when 'Date'
-        Date.parse(raw_value)
-      when 'DateTime'
-        DateTime.parse(raw_value)
-      when 'Time'
-        Time.parse(raw_value)
-      when 'TrueClass'
-        true
-      when 'FalseClass'
-        false
-      when 'NilClass'
-        nil
-      else
-        raise Hypostasis::Errors::UnknownValueType
-    end
-  end
 end
