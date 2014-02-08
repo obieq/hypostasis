6017b8cc31c3d70624728eec5ec6d278be9d1611
jamesthompson <jamest@thereadyproject.com>
2014-02-07 21:58:28 -0700

    Removed unused Hypostasis::Key and Hypostasis::KeyPath classes.

--- lib/hypostasis.rb
+++ lib/hypostasis.rb
@@ -16,8 +16,6 @@ require 'hypostasis/version'
 require 'hypostasis/errors'
 
 require 'hypostasis/tuple'
-require 'hypostasis/key_path'
-require 'hypostasis/key'
 require 'hypostasis/connection'
 require 'hypostasis/data_models'
 require 'hypostasis/namespace'
